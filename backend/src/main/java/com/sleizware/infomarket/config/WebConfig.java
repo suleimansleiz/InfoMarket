49946f9a44b643acf70fcc38a2aad7e14653c728
Suleiman Sleiz <suleimansleiz@gmail.com>
2025-09-17 17:22:39 +0300

    App Intergration

--- backend/src/main/java/com/sleizware/infomarket/config/WebConfig.java
+++ backend/src/main/java/com/sleizware/infomarket/config/WebConfig.java
@@ -9,19 +9,17 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig {
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("https://infomarket.netlify.app")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                        .allowedHeaders("*");
-            }
-        };
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("https://infomarket.netlify.app")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
+
 
     @Bean
     public RestTemplate restTemplate() {
